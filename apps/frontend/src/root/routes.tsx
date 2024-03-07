c64ef601159ee39ec0066a02b2aa35ee8552c2fe
Jakub Wesołowski <kuba.wesolowski@sofomo.com>
2024-03-07 12:09:10 +0100

    Init integrated NX workspace with basic react app

---
+++ apps/frontend/src/root/routes.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+// TODO: Should probably replace with hash router to work with NetSuite
+export default createBrowserRouter([
+  {
+    path: '/',
+    element: <div>App</div>,
+  },
+]);
