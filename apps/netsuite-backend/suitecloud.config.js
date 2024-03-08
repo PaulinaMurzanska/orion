1120b4c461fa5d49967ab1f36f7a71a1c048c45e
Stefan Holodnick <stefan.holodnick@suitecentric.com>
2024-03-08 13:53:59 -0500

    data-to-json-sif-edit: added runner

--- apps/netsuite-backend/suitecloud.config.js
+++ apps/netsuite-backend/suitecloud.config.js
@@ -1,4 +1,16 @@
+const SuiteCloudJestUnitTestRunner = require('@oracle/suitecloud-unit-testing/services/SuiteCloudJestUnitTestRunner')
+
 module.exports = {
 	defaultProjectFolder: "src",
-	commands: {}
+	commands: {
+    'project:deploy': {
+      beforeExecuting: async args => {
+        // await SuiteCloudJestUnitTestRunner.run({
+        //   testRegex: './__tests__/.+\.test\.js$'
+        // });
+
+        return args;
+      }
+    }
+  }
 };
