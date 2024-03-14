336991561cf29ca8d7b826edb0c46038d6e7648b
Jakub Wesolowski <kubawesolowski@me.com>
2024-03-14 08:33:00 +0100

    Fix: Jest config nx failure fix|

--- apps/netsuite-backend/jest.config.js
+++ apps/netsuite-backend/jest.config.js
@@ -1,11 +1,19 @@
-const SuiteCloudJestConfiguration = require("@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration");
-const cliConfig = require("./suitecloud.config");
+const SuiteCloudJestConfiguration = require('@oracle/suitecloud-unit-testing/jest-configuration/SuiteCloudJestConfiguration');
+const cliConfig = require('./suitecloud.config');
 
 const nsExport = SuiteCloudJestConfiguration.build({
-	projectFolder: cliConfig.defaultProjectFolder,
-	projectType: SuiteCloudJestConfiguration.ProjectType.ACP
+  projectFolder: cliConfig.defaultProjectFolder,
+  projectType: SuiteCloudJestConfiguration.ProjectType.ACP,
 });
 
 module.exports = {
-  ...nsExport
-}
+  ...nsExport,
+  displayName: 'backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/backend',
+};
