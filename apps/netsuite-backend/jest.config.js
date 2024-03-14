932d0cbee602e76a7e737eb9a9b3ecd6eab2789b
Stefan Holodnick <stefan.holodnick@suitecentric.com>
2024-03-14 11:59:35 -0400

    jest-build-config-edit: added package.json and modified jest config

--- apps/netsuite-backend/jest.config.js
+++ apps/netsuite-backend/jest.config.js
@@ -11,9 +11,6 @@ module.exports = {
   displayName: 'backend',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  moduleFileExtensions: ['js', 'html'],
   coverageDirectory: '../../coverage/apps/backend',
 };
