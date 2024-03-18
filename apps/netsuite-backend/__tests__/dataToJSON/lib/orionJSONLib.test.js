914e8c34a8fb1fcc38405c12a68738d90932a5da
Stefan Holodnick <stefan.holodnick@suitecentric.com>
2024-03-18 08:36:55 -0400

    data-to-json-edit: added unit tests

--- apps/netsuite-backend/__tests__/dataToJSON/lib/orionJSONLib.test.js
+++ apps/netsuite-backend/__tests__/dataToJSON/lib/orionJSONLib.test.js
@@ -12,12 +12,18 @@ beforeEach(() => {
 })
 
 const testSIFile = fs.readFileSync(path.resolve(__dirname, '../500linesHMIKNO-CAPStudio.sif'), 'utf8')
+const testSIFile2 = fs.readFileSync(path.resolve(__dirname, '../2000linesCapStudio.sif'), 'utf8')
 
 const testXMLFile = fs.readFileSync(path.resolve(__dirname, '../OFDA XML 4.0.xml'), 'utf16le')
 
 describe('orionJSONLib - getFileType - File Type Check', () => {
-  it('is sif file', () => {
+  it('CAP Studio - is sif file', () => {
     const fileName = '500linesHMIKNO-CAPStudio.sif'
+    const extensionName = orionJSONLib.getFileType(fileName)
+    expect(extensionName).toBe('sif')
+  })
+  it('CAP Studio Alt - is sif file', () => {
+    const fileName = '2000linesCapStudio.sif'
     const extensionName = orionJSONLib.getFileType(fileName)
     expect(extensionName).toBe('sif')
   })
@@ -49,10 +55,16 @@ describe('orionJSONLib - findDefLinesByType - Find File Definition by Type', () 
 })
 
 describe('orionJSONLib - findTextLoops - Find Text Loops in File', () => {
-  it('return array of line strings for sif', () => {
+  it('CAP Studio - return array of line strings for sif', () => {
     const fileType = 'sif'
     const filteredFileDefs = orionJSONLib.findDefLinesByType(fileDefs, fileType)
     const textLoops = orionJSONLib.findTextLoops(filteredFileDefs, testSIFile, /^(.+)=/, null, 1)
+    expect(textLoops.length).toBeGreaterThan(0)
+  })
+  it('CAP Studio Alt - return array of line strings for sif', () => {
+    const fileType = 'sif'
+    const filteredFileDefs = orionJSONLib.findDefLinesByType(fileDefs, fileType)
+    const textLoops = orionJSONLib.findTextLoops(filteredFileDefs, testSIFile2, /^(.+)=/, null, 1)
     expect(textLoops.length).toBeGreaterThan(0)
   })
   it('return array of line strings for xml', () => {
@@ -64,10 +76,17 @@ describe('orionJSONLib - findTextLoops - Find Text Loops in File', () => {
 })
 
 describe('orionJSONLib - matchDefinition - Find Matching Definition', () => {
-  it('finds matching definition for sif', () => {
+  it('CAP Studio - finds matching definition for sif', () => {
     const fileType = 'sif'
     const filteredFileDefs = orionJSONLib.findDefLinesByType(fileDefs, fileType)
     const textLoops = orionJSONLib.findTextLoops(filteredFileDefs, testSIFile, /^(.+)=/, null, 1)
+    const definition = orionJSONLib.matchDefinition(filteredFileDefs, textLoops, failureLimit)
+    expect(definition.name).toBe('CAP Studio')
+  })
+  it('CAP Studio Alt - finds matching definition for sif', () => {
+    const fileType = 'sif'
+    const filteredFileDefs = orionJSONLib.findDefLinesByType(fileDefs, fileType)
+    const textLoops = orionJSONLib.findTextLoops(filteredFileDefs, testSIFile2, /^(.+)=/, null, 1)
     const definition = orionJSONLib.matchDefinition(filteredFileDefs, textLoops, failureLimit)
     expect(definition.name).toBe('CAP Studio')
   })
@@ -81,7 +100,33 @@ describe('orionJSONLib - matchDefinition - Find Matching Definition', () => {
 })
 
 describe('orionJSONLib - generateLineJSON - Generate Line JSON', () => {
-  it('generates line JSON for sif', () => {
+  it('CAP Studio - generates line JSON for sif', () => {
+    const fileType = 'sif'
+    const filteredFileDefs = orionJSONLib.findDefLinesByType(fileDefs, fileType)
+    const textLoops = orionJSONLib.findTextLoops(filteredFileDefs, testSIFile, /^(.+)=/, null, 1)
+    const definition = orionJSONLib.matchDefinition(filteredFileDefs, textLoops, failureLimit)
+
+    let idsGenerated = []
+
+    const lineJSON = orionJSONLib.generateLineJSON(definition, textLoops, outDef)
+
+    // check for existance of item in array
+    expect(idsGenerated).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(lineJSON.item.items[0].item)
+      ])
+    )
+
+    // check for existance of item in array
+    expect(idsGenerated).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(lineJSON.item.items[0].povendor)
+      ])
+    )
+
+    expect(lineJSON.item.items.length).toBeGreaterThan(0)
+  })
+  it('CAP Studio Alt - generates line JSON for sif', () => {
     const fileType = 'sif'
     const filteredFileDefs = orionJSONLib.findDefLinesByType(fileDefs, fileType)
     const textLoops = orionJSONLib.findTextLoops(filteredFileDefs, testSIFile, /^(.+)=/, null, 1)
