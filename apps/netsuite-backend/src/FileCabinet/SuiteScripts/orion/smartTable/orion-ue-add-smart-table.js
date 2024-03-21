69bcf80599001f6764bc93832e5dae4c4d8d5dfd
Stefan Holodnick <stefan.holodnick@suitecentric.com>
2024-03-21 12:47:45 -0400

    C000046276-28-script-generate-bom-import-edit: fixed to display tab

--- apps/netsuite-backend/src/FileCabinet/SuiteScripts/orion/smartTable/orion-ue-add-smart-table.js
+++ apps/netsuite-backend/src/FileCabinet/SuiteScripts/orion/smartTable/orion-ue-add-smart-table.js
@@ -4,6 +4,13 @@
  * @NModuleScope SameAccount
  */
 define(['N/ui/serverWidget'], (serverWidget) => {
+  /**
+   * Executes before a record is loaded in the user interface.
+   *
+   * @param {Object} context - The context object containing information about the current record and user event type.
+   * @param {string} context.type - The user event type. Possible values are: CREATE, COPY, VIEW, EDIT, DELETE, XEDIT, APPROVE, REJECT, CANCEL, PACK, SHIP, INVOICE, PRINT, EMAIL, QUICKVIEW, FIELDCHANGED, FIELDAPPROVED, FIELDINIT, FIELDENTER, FIELDEXIT, LINEINIT, LINEENTER, LINEEXIT, SUBLISTADD, SUBLISTDELETE, SUBLISTUPDATE.
+   * @param {N/ui/serverWidget.Form} context.form - The NetSuite form object.
+   */
   const beforeLoad = (context) => {
     if (context.type === context.UserEventType.VIEW) {
       const form = context.form
@@ -18,17 +25,13 @@ define(['N/ui/serverWidget'], (serverWidget) => {
       const htmlField = form.addField({
         id: 'custpage_smart_table_html',
         type: serverWidget.FieldType.INLINEHTML,
-        label: 'Custom HTML'
+        label: 'Custom HTML',
+        container: 'custpage_smart_table_tab'
       })
       htmlField.updateLayoutType({
         layoutType: serverWidget.FieldLayoutType.OUTSIDEABOVE
       })
       htmlField.defaultValue = '<p>React Code Goes Here</p>'
-
-      // Set the tab as the default tab
-      form.updateDefaultValues({
-        tab: tab
-      })
     }
   }
 
