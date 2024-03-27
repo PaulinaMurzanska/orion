3674cef9fa16af4d1911c280363c051008cc9f5a
Stefan Holodnick <stefan.holodnick@suitecentric.com>
2024-03-27 00:25:26 -0400

    C000046276-43-script-bom-update-transaci-edit: added string id to transaction

--- apps/netsuite-backend/src/FileCabinet/SuiteScripts/orion/bomImport/orion-ue-bom-logic.js
+++ apps/netsuite-backend/src/FileCabinet/SuiteScripts/orion/bomImport/orion-ue-bom-logic.js
@@ -4,10 +4,12 @@
  * @NModuleScope SameAccount
  */
 
-define(['N/record'], (record) => {
+define(['N/record', 'N/query', 'N/ui/serverWidget'], (record, query, serverWidget) => {
   let createdString
 
   const beforeLoad = async (context) => {
+    const loggerTitle = 'beforeLoad'
+    log.debug(loggerTitle, `context: ${JSON.stringify(context)}`)
     if (context.type === context.UserEventType.CREATE) {
       await setBeforeDataValue(context)
     }
@@ -19,18 +21,64 @@ define(['N/record'], (record) => {
     }
   }
 
-  const setBeforeDataValue = (context) => {
-    const currentRecord = context.newRecord
-    const inlineHtmlField = currentRecord.getField({ fieldId: 'custpage_created_data_code_field' })
+  /**
+   * Sets the before data value for the given context.
+   * @param {Object} context - The context object.
+   * @returns {Promise<void>} - A Promise that resolves when the before data value is set.
+   */
+  const setBeforeDataValue = async (context) => {
+    const loggerTitle = 'setBeforeDataValue'
+    try {
+      const form = context.form
+      const inlineHtmlField = form.addField({
+        id: 'custpage_created_data_code_field',
+        type: serverWidget.FieldType.INLINEHTML,
+        label: 'Created Data Code'
+      })
 
-    createdString = generateRandomString(20)
-    inlineHtmlField.defaultValue = `<span data-create-id="${createdString}"></span>`
+      const createdString = generateRandomString(20)
+      inlineHtmlField.defaultValue = `<span id="created-string-id" data-create-id="${createdString}"></span>`
+    } catch (e) {
+      log.error(loggerTitle, `Error: ${e}`)
+    }
   }
 
-  const setAfterDataValue = (context) => {
+  /**
+   * Sets the after data value for the given context.
+   * 
+   * @param {Object} context - The context object containing the new record.
+   */
+  const setAfterDataValue = async (context) => {
+    const loggerTitle = 'setAfterDataValue'
     const currentRecord = context.newRecord
     const transactionID = currentRecord.id
 
+    const getBOMRecordsQL = `
+      SELECT id
+      FROM customrecord_orion_bom_import
+      WHERE custrecord_orion_bom_intializaiton_ident = '${createdString}'
+    `
+
+    const bomRecordsFound = await new Promise(resolve => { resolve(query.runSuiteQL(getBOMRecordsQL).asMappedResults()) })
+
+    log.debug(loggerTitle, `bomRecordsFound: ${JSON.stringify(bomRecordsFound)}`)
+
+    if (bomRecordsFound?.length > 0) {
+      for (let bomRecord of bomRecordsFound) {
+        const bomRecordID = bomRecord.id
+        const bomRecordObj = record.load({
+          type: 'customrecord_orion_bom_import',
+          id: bomRecordID
+        })
+
+        bomRecordObj.setValue({
+          fieldId: 'custrecord_bom_import_transaction',
+          value: transactionID
+        })
+
+        bomRecordObj.save()
+      }
+    }
   }
 
   const generateRandomString = (length) => {
@@ -44,6 +92,7 @@ define(['N/record'], (record) => {
   }
 
   return {
-    beforeLoad: beforeLoad
+    beforeLoad: beforeLoad,
+    afterSubmit: afterSubmit
   }
 })
