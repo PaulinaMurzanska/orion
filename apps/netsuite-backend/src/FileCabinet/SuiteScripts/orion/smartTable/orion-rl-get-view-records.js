a582f7144f40065e16e2d3e43907750fa35b684e
Stefan Holodnick <stefan.holodnick@suitecentric.com>
2024-04-11 14:47:10 -0400

    C000046276-57-get-table-views-edit: adjustments to fix response

--- apps/netsuite-backend/src/FileCabinet/SuiteScripts/orion/smartTable/orion-rl-get-view-records.js
+++ apps/netsuite-backend/src/FileCabinet/SuiteScripts/orion/smartTable/orion-rl-get-view-records.js
@@ -6,33 +6,37 @@
 
 define(['N/query', 'N/runtime', 'N/error'], (query, runtime, error) => {
   
-  const get = async (context) => {
+  const get = (context) => {
     const loggerTitle = 'get'
-
-    let fieldString, valueString
 
     log.debug(loggerTitle, `context: ${JSON.stringify(context)}`)
 
     const currentUser = runtime.getCurrentUser()
-    const role = currentUser.role
+    const currentRole = currentUser.role
 
-    log.debug(loggerTitle, `currentUser: ${JSON.stringify(currentUser)}, role: ${JSON.stringify(role)}`)
+    log.debug(loggerTitle, `currentUser: ${currentUser}, role: ${currentRole}`)
     
     // Create a SuiteQL query to find the custom record
     const findRecordsSuiteQL = `
-      SELECT id
+      SELECT *
       FROM customrecord_orion_smarttable_view
-      WHERE (BUILTIN.MNFILTER(custrecord_orion_smarttable_roles, 'MN_INCLUDE', '', 'FALSE', NULL, ${Number(role)}) = 'T' || custrecord_orion_smarttable_current_user = ${currentUser.id}) AND isinactive = 'F'
+      WHERE (BUILTIN.MNFILTER(custrecord_orion_smarttable_roles, 'MN_INCLUDE', '', 'FALSE', NULL, ${Number(currentRole)}) = 'T' OR custrecord_orion_smarttable_current_user = ${currentUser.id}) AND isinactive = 'F'
     `
+
+    log.debug(loggerTitle, `findRecordsSuiteQL: ${findRecordsSuiteQL}`)
     
-    const results = await new Promise(resolve => { resolve(query.runSuiteQL({ query: findRecordsSuiteQL }).asMappedResults()) })
+    const results = query.runSuiteQL({ query: findRecordsSuiteQL }).asMappedResults()
 
     log.debug(loggerTitle, `results: ${JSON.stringify(results)}`)
 
     if (results?.length > 0) {
       return { message: "SUCCESS: Import Records Found", tableViews: results }
     } else {
-      throw { message: "ERROR: No Import Records Found" }
+      throw error.create({
+        name: 'ORION_NO_VIEW_RECORDS_FOUND',
+        message: 'No view Records Found',
+        notifyOff: false
+      })
     }
   }
 
