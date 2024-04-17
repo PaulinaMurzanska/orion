05ee810f8f50b677409710420f6a4be021c123da
Paulina Murzanska <paulina.murzanska@sofomo.com>
2024-04-17 16:46:53 +0200

    create color variables files

---
+++ apps/frontend/src/styles/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  blue_primary_hover: '#144fcf',
+  blue_primary: '#2563eb',
+  cyan_400: '#26C2CC',
+  dark_blue: '#344767',
+  grey_200: '#E4E4E',
+  grey_300: '#7B809A',
+  grey_400: '#989898',
+  grey_700: '#434343',
+  lime_500: '#76D748',
+};
+
+export default colors;
